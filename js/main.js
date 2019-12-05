573a89b50f56ab5e0515b3a64e071064da0571da
Alexis <alkryzhanov@hotmail.com>
2019-12-05 19:38:56 +0800

    First commit!

---
+++ js/main.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function($) {
+  $('.header-list__btn').on('click', function() {
+    $('.header-list').slideToggle()
+  });
+  var index = $('.social-list__link--facebook').index();
+
+});
