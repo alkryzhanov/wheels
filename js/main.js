ba7d96eb29e954c9a20ecb51c820dfa4ced1da1d
Alexis <alkryzhanov@hotmail.com>
2020-06-06 18:20:44 +0300

    First commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
-jQuery(document).ready(function($) {
-  $('.header-list__btn').on('click', function() {
-    $('.header-list').slideToggle()
+jQuery(document).ready(function ($) {
+  $('.header-list__btn').on('click', function () {
+    $('.header-list').toggleClass('header-list--closed');
+    if ($(this)) {
+      $('.header-list').addClass('header-list--opened');
+    } else {
+      $('.header-list').remmoveClass('header-list--closed');
+    }
+    $(this).toggleClass('header-list__btn--closed header-list__btn--opened');
   });
-  var index = $('.social-list__link--facebook').index();
-
+  $('#about-us').click(function () {
+    $('html, body').animate({
+      scrollTop: ($('.about-us').first().offset().top)
+    }, 1000);
+  });
 });
